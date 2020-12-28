dd0613e805ec03234028c3a110c97ff4569f7e3b
Jay Shapiro <jas2dp@virginia.edu>
2020-12-28 15:51:12 -0500

    Add files via upload

--- app/translate.py
+++ app/translate.py
@@ -9,13 +9,15 @@ def translate(text, source_language, dest_language):
 
     auth = {
         'Ocp-Apim-Subscription-Key': current_app.config['MS_TRANSLATOR_KEY'],
-        'Ocp-Apim-Subscription-Region': 'eastus'
-    }
+        'Ocp-Apim-Subscription-Region': 'eastus'}
 
     r = requests.post(
         'https://api.cognitive.microsofttranslator.com'
         '/translate?api-version=3.0&from={}&to={}'.format(
             source_language, dest_language), headers=auth, json=[{'Text': text}])
+
+    # import pdb
+    # pdb.set_trace()
 
     if r.status_code != 200:
         return _('Error: the translation service failed.')
